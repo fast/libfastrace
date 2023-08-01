3f258a4a303727c827ab50e46e045aab0e17e9ac
Wenbo Zhang <ethercflow@gmail.com>
2023-08-01 21:55:42 +0800

    chore(minitrace): export mods
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

---
+++ minitrace/src/lib.rs
@@ -0,0 +1,28 @@
+// Copyright 2023 Wenbo Zhang. Licensed under Apache-2.0.
+
+// We have a lot of c-types in here, stop warning about their names!
+#![allow(non_camel_case_types)]
+// fmt::Debug isn't helpful on FFI types
+#![allow(missing_debug_implementations)]
+// unreable_pub warns `#[no_mangle] pub extern fn` in private mod.
+#![allow(unreachable_pub)]
+
+//! # mimitrace C API
+
+#[macro_use]
+mod macros;
+
+pub mod collector;
+mod local;
+mod span;
+
+pub use self::local::*;
+
+/// cbindgen:ignore
+static VERSION_CSTR: &str = concat!(env!("CARGO_PKG_VERSION"), "\0");
+
+ffi_fn! {
+    fn mtr_c_ver() -> *const libc::c_char {
+        VERSION_CSTR.as_ptr() as _
+    } ?= std::ptr::null()
+}
