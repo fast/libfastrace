bc496684ff3b54f83acf0f6d2751217c731e875c
Wenbo Zhang <ethercflow@gmail.com>
2023-08-21 04:21:20 +0000

    feat(lib): add local span method
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

--- minitrace/src/lib.rs
+++ minitrace/src/lib.rs
@@ -1,6 +1,6 @@
 // Copyright 2023 Wenbo Zhang. Licensed under Apache-2.0.
 
-use minitrace::prelude::Span;
+use minitrace::prelude::{LocalSpan, Span};
 use std::mem::transmute;
 
 use self::ffi::*;
@@ -19,6 +19,10 @@ mod ffi {
         _padding: [u8; 24],
     }
 
+    struct mtr_loc_span {
+        _padding: [u8; 24],
+    }
+
     struct mtr_loc_spans {
         _padding: [u8; 8],
     }
@@ -33,6 +37,8 @@ mod ffi {
         fn mtr_set_loc_par_to_span(span: &mtr_span) -> mtr_loc_par_guar;
 
         fn mtr_push_child_spans_to_cur(span: &mtr_span, local_span: mtr_loc_spans);
+
+        fn mtr_create_loc_span_enter(name: &'static str) -> mtr_loc_span;
     }
 }
 
@@ -54,4 +60,8 @@ fn mtr_set_loc_par_to_span(span: &mtr_span) -> mtr_loc_par_guar {
 
 fn mtr_push_child_spans_to_cur(span: &mtr_span, local_span: mtr_loc_spans) {
     unsafe { transmute::<&mtr_span, &Span>(span).push_child_spans(transmute(local_span)) }
+}
+
+fn mtr_create_loc_span_enter(name: &'static str) -> mtr_loc_span {
+    unsafe { transmute(LocalSpan::enter_with_local_parent(name)) }
 }
