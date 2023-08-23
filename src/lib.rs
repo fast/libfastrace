0a973fc4a3d840444e57018be1c6fa8325a3f1b9
Wenbo Zhang <ethercflow@gmail.com>
2023-08-23 07:15:31 +0000

    feat(lib): grue more trace methods and modify dir structure
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,73 +2,95 @@
 
 use minitrace::{
     collector::{Config, ConsoleReporter},
-    local::LocalCollector,
-    prelude::{LocalSpan, Span},
+    local::{LocalCollector, LocalParentGuard},
+    prelude::{LocalSpan, Span, SpanContext},
 };
+use minitrace_opentelemetry::OpenTelemetryReporter;
 use std::{mem::transmute, time::Duration};
 
 use self::ffi::*;
 
 #[cxx::bridge]
 mod ffi {
+
+    #[namespace = "ffi"]
     struct mtr_span_ctx {
         _padding: [u8; 24],
     }
 
+    #[namespace = "ffi"]
     struct mtr_span {
         _padding: [u8; 128],
     }
 
+    #[namespace = "ffi"]
     struct mtr_loc_par_guar {
         _padding: [u8; 24],
     }
 
+    #[namespace = "ffi"]
     struct mtr_loc_span {
         _padding: [u8; 24],
     }
 
+    #[namespace = "ffi"]
     struct mtr_loc_spans {
         _padding: [u8; 8],
     }
 
+    #[namespace = "ffi"]
     struct mtr_loc_coll {
         _padding: [u8; 16],
     }
 
+    #[namespace = "ffi"]
     struct mtr_coll_cfg {
         _padding: [u8; 48],
     }
 
+    #[namespace = "ffi"]
     struct mtr_otel_rptr {
         _padding: [u8; 192],
     }
 
+    #[namespace = "ffi"]
     struct mtr_otlp_exp_cfg {
         _padding: [u8; 48],
     }
 
+    #[namespace = "ffi"]
     struct mtr_otlp_grpcio_cfg {
         _padding: [u8; 112],
     }
 
+    #[namespace = "minitrace_glue"]
     extern "Rust" {
+        fn mtr_create_rand_span_ctx() -> mtr_span_ctx;
+
         fn mtr_create_root_span(name: &'static str, parent: mtr_span_ctx) -> mtr_span;
 
         fn mtr_create_child_span_enter(name: &'static str, parent: &mtr_span) -> mtr_span;
 
         fn mtr_create_child_span_enter_loc(name: &'static str) -> mtr_span;
 
+        fn mtr_destroy_span(span: mtr_span);
+
         fn mtr_set_loc_par_to_span(span: &mtr_span) -> mtr_loc_par_guar;
+
+        fn mtr_destroy_loc_par_guar(guard: mtr_loc_par_guar);
 
         fn mtr_push_child_spans_to_cur(span: &mtr_span, local_span: mtr_loc_spans);
 
         fn mtr_create_loc_span_enter(name: &'static str) -> mtr_loc_span;
+
+        fn mtr_destroy_loc_span(span: mtr_loc_span);
 
         fn mtr_start_loc_coll() -> mtr_loc_coll;
 
         fn mtr_collect_loc_spans(lc: mtr_loc_coll) -> mtr_loc_spans;
 
         fn mtr_create_def_coll_cfg() -> mtr_coll_cfg;
+
         fn mtr_set_max_spans_per_trace(cfg: mtr_coll_cfg, mspt: usize) -> mtr_coll_cfg;
 
         fn mtr_set_batch_report_interval(cfg: mtr_coll_cfg, bri: u64) -> mtr_coll_cfg;
@@ -83,8 +105,16 @@ mod ffi {
 
         fn mtr_create_otel_rptr(cfg: mtr_otlp_exp_cfg, gcfg: mtr_otlp_grpcio_cfg) -> mtr_otel_rptr;
 
+        fn mtr_destroy_otel_rptr(rptr: mtr_otel_rptr);
+
+        fn mtr_set_otel_rptr(rptr: mtr_otel_rptr, cfg: mtr_coll_cfg);
+
         fn mtr_flush();
     }
+}
+
+fn mtr_create_rand_span_ctx() -> mtr_span_ctx {
+    unsafe { transmute(SpanContext::random()) }
 }
 
 fn mtr_create_root_span(name: &'static str, parent: mtr_span_ctx) -> mtr_span {
@@ -99,8 +129,16 @@ fn mtr_create_child_span_enter_loc(name: &'static str) -> mtr_span {
     unsafe { transmute(Span::enter_with_local_parent(name)) }
 }
 
+fn mtr_destroy_span(span: mtr_span) {
+    unsafe { drop(transmute::<mtr_span, Span>(span)) }
+}
+
 fn mtr_set_loc_par_to_span(span: &mtr_span) -> mtr_loc_par_guar {
     unsafe { transmute(transmute::<&mtr_span, &Span>(span).set_local_parent()) }
+}
+
+fn mtr_destroy_loc_par_guar(guard: mtr_loc_par_guar) {
+    unsafe { drop(transmute::<mtr_loc_par_guar, LocalParentGuard>(guard)) }
 }
 
 fn mtr_push_child_spans_to_cur(span: &mtr_span, local_span: mtr_loc_spans) {
@@ -109,6 +147,10 @@ fn mtr_push_child_spans_to_cur(span: &mtr_span, local_span: mtr_loc_spans) {
 
 fn mtr_create_loc_span_enter(name: &'static str) -> mtr_loc_span {
     unsafe { transmute(LocalSpan::enter_with_local_parent(name)) }
+}
+
+fn mtr_destroy_loc_span(span: mtr_loc_span) {
+    unsafe { drop(transmute::<mtr_loc_span, LocalSpan>(span)) }
 }
 
 fn mtr_start_loc_coll() -> mtr_loc_coll {
@@ -178,6 +220,19 @@ fn mtr_create_otel_rptr(cfg: mtr_otlp_exp_cfg, gcfg: mtr_otlp_grpcio_cfg) -> mtr
                 None,
             ),
         ))
+    }
+}
+
+fn mtr_destroy_otel_rptr(rptr: mtr_otel_rptr) {
+    unsafe { drop(transmute::<mtr_otel_rptr, OpenTelemetryReporter>(rptr)) }
+}
+
+fn mtr_set_otel_rptr(rptr: mtr_otel_rptr, cfg: mtr_coll_cfg) {
+    unsafe {
+        minitrace::set_reporter(
+            transmute::<mtr_otel_rptr, OpenTelemetryReporter>(rptr),
+            transmute(cfg),
+        )
     }
 }
 
---
+++ src/minitrace_c.cpp
@@ -0,0 +1,208 @@
+// Copyright 2023 Wenbo Zhang. Licensed under Apache-2.0.
+
+#include "minitrace_c.h"
+
+extern "C" {
+
+union _mtr_span_ctx {
+  mtr_span_ctx c;
+  ffi::mtr_span_ctx f;
+};
+
+union _mtr_span {
+  mtr_span c;
+  ffi::mtr_span f;
+};
+
+union _mtr_loc_par_guar {
+  mtr_loc_par_guar c;
+  ffi::mtr_loc_par_guar f;
+};
+
+union _mtr_loc_span {
+  mtr_loc_span c;
+  ffi::mtr_loc_span f;
+};
+
+union _mtr_loc_coll {
+  mtr_loc_coll c;
+  ffi::mtr_loc_coll f;
+};
+
+union _mtr_loc_spans {
+  mtr_loc_spans c;
+  ffi::mtr_loc_spans f;
+};
+
+union _mtr_coll_cfg {
+  mtr_coll_cfg c;
+  ffi::mtr_coll_cfg f;
+};
+
+union _mtr_otlp_exp_cfg {
+  mtr_otlp_exp_cfg c;
+  ffi::mtr_otlp_exp_cfg f;
+};
+
+union _mtr_otlp_grpcio_cfg {
+  mtr_otlp_grpcio_cfg c;
+  ffi::mtr_otlp_grpcio_cfg f;
+};
+
+union _mtr_otel_rptr {
+  mtr_otel_rptr c;
+  ffi::mtr_otel_rptr f;
+};
+
+mtr_span_ctx mtr_create_rand_span_ctx() {
+  union _mtr_span_ctx _msc = {
+      .f = minitrace_glue::mtr_create_rand_span_ctx(),
+  };
+  return std::move(_msc.c);
+}
+
+mtr_span mtr_create_root_span(const char *name, mtr_span_ctx parent) {
+  union _mtr_span _ms = {
+      .f = minitrace_glue::mtr_create_root_span(
+          name, *(ffi::mtr_span_ctx *)(&parent)),
+  };
+  return std::move(_ms.c);
+}
+
+mtr_span mtr_create_child_span_enter(const char *name, mtr_span const *parent) {
+  union _mtr_span _p = {
+      .c = *parent,
+  };
+  union _mtr_span _ms = {
+      .f = minitrace_glue::mtr_create_child_span_enter(name, _p.f),
+  };
+  return std::move(_ms.c);
+}
+
+mtr_span mtr_create_child_span_enter_loc(const char *name) {
+  union _mtr_span _ms = {
+      .f = minitrace_glue::mtr_create_child_span_enter_loc(name),
+  };
+  return std::move(_ms.c);
+}
+
+void mtr_destroy_span(mtr_span span) {
+  minitrace_glue::mtr_destroy_span(*(ffi::mtr_span *)(&span));
+}
+
+mtr_loc_par_guar mtr_set_loc_par_to_span(mtr_span const *span) {
+  union _mtr_span _p = {
+      .c = *span,
+  };
+  union _mtr_loc_par_guar _mlpg = {
+      .f = minitrace_glue::mtr_set_loc_par_to_span(_p.f),
+  };
+  return std::move(_mlpg.c);
+}
+
+void mtr_destroy_loc_par_guar(mtr_loc_par_guar guard) {
+  minitrace_glue::mtr_destroy_loc_par_guar(*(ffi::mtr_loc_par_guar *)(&guard));
+}
+
+void mtr_push_child_spans_to_cur(mtr_span const *span,
+                                 mtr_loc_spans local_span) {
+  union _mtr_span _p = {
+      .c = *span,
+  };
+  minitrace_glue::mtr_push_child_spans_to_cur(
+      _p.f, *(ffi::mtr_loc_spans *)(&local_span));
+}
+
+mtr_loc_span mtr_create_loc_span_enter(const char *name) {
+  union _mtr_loc_span _mls = {
+      .f = minitrace_glue::mtr_create_loc_span_enter(name),
+  };
+  return std::move(_mls.c);
+}
+
+void mtr_destroy_loc_span(mtr_loc_span span) {
+  minitrace_glue::mtr_destroy_loc_span(*(ffi::mtr_loc_span *)(&span));
+}
+
+mtr_loc_coll mtr_start_loc_coll(void) {
+  union _mtr_loc_coll _mlc = {
+      .f = minitrace_glue::mtr_start_loc_coll(),
+  };
+  return std::move(_mlc.c);
+}
+
+mtr_loc_spans mtr_collect_loc_spans(mtr_loc_coll lc) {
+  union _mtr_loc_spans _mls = {
+      .f = minitrace_glue::mtr_collect_loc_spans(*(ffi::mtr_loc_coll *)(&lc)),
+  };
+  return std::move(_mls.c);
+}
+
+mtr_coll_cfg mtr_create_def_coll_cfg(void) {
+  union _mtr_coll_cfg _mcc = {
+      .f = minitrace_glue::mtr_create_def_coll_cfg(),
+  };
+  return std::move(_mcc.c);
+}
+
+mtr_coll_cfg mtr_set_max_spans_per_trace(mtr_coll_cfg cfg, size_t mspt) {
+  union _mtr_coll_cfg _mcc = {
+      .f = minitrace_glue::mtr_set_max_spans_per_trace(
+          *(ffi::mtr_coll_cfg *)(&cfg), mspt),
+  };
+  return std::move(_mcc.c);
+}
+
+mtr_coll_cfg mtr_set_batch_report_interval(mtr_coll_cfg cfg, uint64_t bri) {
+  union _mtr_coll_cfg _mcc = {
+      .f = minitrace_glue::mtr_set_batch_report_interval(
+          *(ffi::mtr_coll_cfg *)(&cfg), bri),
+  };
+  return std::move(_mcc.c);
+}
+
+mtr_coll_cfg mtr_set_report_max_spans(mtr_coll_cfg cfg, size_t brms) {
+  union _mtr_coll_cfg _mcc = {
+      .f = minitrace_glue::mtr_set_report_max_spans(
+          *(ffi::mtr_coll_cfg *)(&cfg), brms),
+  };
+  return std::move(_mcc.c);
+}
+
+void mtr_set_cons_rptr(void) { minitrace_glue::mtr_set_cons_rptr(); }
+
+mtr_otlp_exp_cfg mtr_create_def_otlp_exp_cfg(void) {
+  union _mtr_otlp_exp_cfg _moec = {
+      .f = minitrace_glue::mtr_create_def_otlp_exp_cfg(),
+  };
+  return std::move(_moec.c);
+}
+
+mtr_otlp_grpcio_cfg mtr_create_def_otlp_grpcio_cfg(void) {
+  union _mtr_otlp_grpcio_cfg _moec = {
+      .f = minitrace_glue::mtr_create_def_otlp_grpcio_cfg(),
+  };
+  return std::move(_moec.c);
+}
+
+mtr_otel_rptr mtr_create_otel_rptr(mtr_otlp_exp_cfg cfg,
+                                   mtr_otlp_grpcio_cfg gcfg) {
+  union _mtr_otel_rptr _mor = {
+      .f = minitrace_glue::mtr_create_otel_rptr(
+          *(ffi::mtr_otlp_exp_cfg *)(&cfg),
+          *(ffi::mtr_otlp_grpcio_cfg *)(&gcfg)),
+  };
+  return std::move(_mor.c);
+}
+
+void mtr_destroy_otel_rptr(mtr_otel_rptr rptr) {
+  minitrace_glue::mtr_destroy_otel_rptr(*(ffi::mtr_otel_rptr *)(&rptr));
+}
+
+void mtr_set_otel_rptr(mtr_otel_rptr rptr, mtr_coll_cfg cfg) {
+  minitrace_glue::mtr_set_otel_rptr(*(ffi::mtr_otel_rptr *)(&rptr),
+                                    *(ffi::mtr_coll_cfg *)(&cfg));
+}
+
+void mtr_flush(void) { minitrace_glue::mtr_flush(); }
+}
