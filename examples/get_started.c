dd5643d395121e593695319aa7ce886eeb181d7a
Wenbo Zhang <wenbo.zhang@iomesh.com>
2024-09-04 14:16:15 +0800

    feat: update get_started to use events properties related API
    Signed-off-by: Wenbo Zhang <wenbo.zhang@iomesh.com>

--- examples/get_started.c
+++ examples/get_started.c
@@ -2,12 +2,27 @@
 
 #include <fastrace_c/fastrace_c.h>
 
+static const char* keys[] = {"pk1", "pk2", "pk3", "pk4", "ck1",
+                             "ck2", "ck3", "ck4", "ck5", "ck6"};
+static const char* vals[] = {"pv1", "pv2", "pv3", "pv4", "cv1",
+                             "cv2", "cv3", "cv4", "cv5", "cv6"};
+
 int main(void) {
   ftr_set_cons_rptr();
   ftr_span_ctx p = ftr_create_rand_span_ctx();
   ftr_span r = ftr_create_root_span("root", p);
+  ftr_add_ent_to_par("parent event", &r, keys, vals, 2);
+  ftr_span_with_prop(&r, "phello", "pworld");
+  ftr_span_with_props(&r, &keys[2], &vals[2], 2);
+
   ftr_loc_par_guar g = ftr_set_loc_par_to_span(&r);
-  ftr_loc_span ls = ftr_create_loc_span_enter("a span");
+
+  ftr_loc_span ls = ftr_create_loc_span_enter("child");
+  ftr_add_ent_to_loc_par("child event", &keys[4], &vals[4], 2);
+  ftr_loc_span_add_prop("chello", "cworld");
+  ftr_loc_span_add_props(&keys[6], &vals[6], 2);
+  ftr_loc_span_with_prop(&ls, "chello2", "cworld2");
+  ftr_loc_span_with_props(&ls, &keys[8], &vals[8], 2);
 
   ftr_destroy_loc_span(ls);
   ftr_destroy_loc_par_guar(g);
--- examples/get_started2.cc
+++ examples/get_started2.cc
@@ -2,12 +2,43 @@
 
 #include <fastrace_c/fastrace_c.h>
 
+#include <array>
+
 int main(void) {
+  std::array<const char*, 10> keys = {"pk1", "pk2", "pk3", "pk4", "ck1",
+                                      "ck2", "ck3", "ck4", "ck5", "ck6"};
+  std::array<const char*, 10> vals = {"pv1", "pv2", "pv3", "pv4", "cv1",
+                                      "cv2", "cv3", "cv4", "cv5", "cv6"};
+
   fastrace_glue::ftr_set_cons_rptr();
   auto p = fastrace_glue::ftr_create_rand_span_ctx();
   auto r = fastrace_glue::ftr_create_root_span("root", p);
+
+  fastrace_glue::ftr_add_ent_to_par(
+      "parent event", r, rust::Slice<const char* const>(keys.data(), 2),
+      rust::Slice<const char* const>(vals.data(), 2));
+
+  fastrace_glue::ftr_span_with_prop(r, "phello", "pworld");
+  fastrace_glue::ftr_span_with_props(
+      r, rust::Slice<const char* const>(keys.data() + 2, 2),
+      rust::Slice<const char* const>(vals.data() + 2, 2));
+
   auto g = fastrace_glue::ftr_set_loc_par_to_span(r);
-  auto ls = fastrace_glue::ftr_create_loc_span_enter("a span");
+
+  auto ls = fastrace_glue::ftr_create_loc_span_enter("child");
+  fastrace_glue::ftr_add_ent_to_loc_par(
+      "child event", rust::Slice<const char* const>(keys.data() + 4, 2),
+      rust::Slice<const char* const>(vals.data() + 4, 2));
+
+  fastrace_glue::ftr_loc_span_add_prop("chello", "cworld");
+  fastrace_glue::ftr_loc_span_add_props(
+      rust::Slice<const char* const>(keys.data() + 6, 2),
+      rust::Slice<const char* const>(vals.data() + 6, 2));
+
+  fastrace_glue::ftr_loc_span_with_prop(ls, "chello2", "cworld2");
+  fastrace_glue::ftr_loc_span_with_props(
+      ls, rust::Slice<const char* const>(keys.data() + 8, 2),
+      rust::Slice<const char* const>(vals.data() + 8, 2));
 
   fastrace_glue::ftr_destroy_loc_span(ls);
   fastrace_glue::ftr_destroy_loc_par_guar(g);
